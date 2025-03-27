7ff2082670a8a34e6e1a37fa88f75f9ff2edbc14
Devisha <157816727+git-devisha@users.noreply.github.com>
2025-03-27 21:19:28 +0530

    kadane's algorithm

--- arrays/kadanesalgo.cpp
+++ arrays/kadanesalgo.cpp
@@ -1,2 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+long long maxSubarraySum(int arr[], int n)
+{
+    long long maxIndex = LLONG_MIN; // Correct initialization
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+
+        // Correct update of maxIndex
+        maxIndex = max(maxIndex, sum);
+
+        // Reset sum if it goes negative
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+    return maxIndex;
+}
+
+int main()
+{
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    long long maxsum = maxSubarraySum(arr, n);
+    cout << "Max sum of subarray is: " << maxsum << endl;
+    return 0;
+}
